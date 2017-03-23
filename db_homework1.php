105a91b620a4be2c04335b65f2dbf435611bf53c
apbelg <apbelg@mail.ru>
2017-03-23 22:43:00 +0300

    Add files via upload

--- db_homework1.php
+++ db_homework1.php
@@ -1,27 +1,25 @@
 <?php
-  $biblio = new PDO('mysql:host=localhost; dbname=global', 'root', '');
-  $sql = 'SELECT * FROM books ';
-  $sql1 = '';
+  $biblio = new PDO('mysql:host=localhost; dbname=global; charset=utf8', '*******', '******');
+  $biblio->query('SET NAMES utf8');
+  $sql = "SELECT * FROM books WHERE isbn LIKE :isbn AND name LIKE :nameBook AND author LIKE :authorBook";
+  $isbn = '';
+  $nameBook = '';
+  $authorBook = '';
   if (isset($_POST['isbn']) && !$_POST['isbn']=='') {
-      $sql1= ' isbn LIKE "%'.$_POST['isbn'].'%"';
+      $isbn = $_POST['isbn'];
   }
   if (isset($_POST['nameBook']) && !$_POST['nameBook']=='') {
-      if ($sql1 !== '') {
-          $sql1 = $sql1.' AND ';
-      }
-      $sql1 = $sql1.' name LIKE "%'.$_POST['nameBook'].'%"';
+      $nameBook = $_POST['nameBook'];
   }
   if (isset($_POST['authorBook']) && !$_POST['authorBook']=='') {
-      if ($sql1 !== '') {
-          $sql1 = $sql1.' AND ';
-      }
-      $sql1 = $sql1.' author LIKE "%'.$_POST['authorBook'].'%"';
+      $authorBook = $_POST['authorBook'];
   }
-  if ($sql1 !== '') {
-      $sql = $sql.' WHERE';
-  }
-  $sql = $sql.$sql1;
-  $result = $biblio->query($sql);
+      $stm = $biblio->prepare($sql);
+      $isbn1 = "%$isbn%";
+      $namebook1 = "%$nameBook%";
+      $authorBook1 = "%$authorBook%";
+      $stm->execute(array ('isbn' => $isbn1, 'nameBook' => $namebook1, 'authorBook' => $authorBook1));
+      $result = $stm->fetchAll();
 ?>
 <!Doctype html>
 <html>
@@ -32,9 +30,9 @@
 </head>
 <h1> Библиотека успешного человека </h1>
 <form method = 'POST'>
-   <input type = 'text' name = 'isbn' value = '<?= $_POST['isbn'] ?>' placeholder = "ISBN">
-   <input type = 'text' name = 'nameBook' value = '<?= $_POST['nameBook'] ?>' placeholder = "Название книги">
-   <input type = 'text' name = 'authorBook'value = '<?= $_POST['authorBook'] ?>' placeholder = "Автор книги">
+   <input type = 'text' name = 'isbn' value = "<?= $isbn ?>" placeholder = "ISBN">
+   <input type = 'text' name = 'nameBook' value = "<?= $nameBook ?>" placeholder = "Название книги">
+   <input type = 'text' name = 'authorBook' value = "<?= $authorBook ?>" placeholder = "Автор книги">
    <button type = 'submit'>Выбрать</button>
 </form>
 <br>
